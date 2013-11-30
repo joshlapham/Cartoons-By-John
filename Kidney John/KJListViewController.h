13e594db27f587c66037c93e6e578a0428279c26
Josh Lapham <josh@joshlapham.com>
2013-12-01 01:26:51 +1100

    Started implementing table view for video list.

---
+++ Kidney John/KJListViewController.h
@@ -0,0 +1,13 @@
+//
+//  KJListViewController.h
+//  Kidney John
+//
+//  Created by jl on 1/12/13.
+//  Copyright (c) 2013 Josh Lapham. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KJListViewController : UITableViewController <UITableViewDelegate, UITableViewDataSource>
+
+@end
