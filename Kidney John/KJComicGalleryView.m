7cddeafda931c56d3335e63027f3b4e88e0da9d5
Josh Lapham <josh@joshlapham.com>
2014-01-04 02:17:35 +1100

    Partially implemented MWPhotoBrowser. Currently using comic thumbnails rather than full-size images, need to review UI as it does not look right in a UITabBarController.

--- Kidney John/KJComicGalleryView.m
+++ Kidney John/KJComicGalleryView.m
@@ -36,30 +36,43 @@
     browser.startOnGrid = YES;
     browser.zoomPhotosToFill = YES;
     //[browser setCurrentPhotoIndex:0];
+    
+    [self fetchComicsForBrowser];
 }
 
 - (void)fetchComicsForBrowser
 {
     NSLog(@"Fetching comics for browser ...");
     
-//    KJComic *aComic = [[KJComic alloc] init];
-//    MWPhoto *comic = [[MWPhoto alloc] init];
-//    comic = [MWPhoto photoWithImage:[UIImage imageWithData:aComic.comicThumbData]];
-//    [comicsForBrowser addObject:comic];
-    
     // Init comics array with results from Core Data
-    comicResults = [NSArray array];
+    comicResults = [[NSArray alloc] init];
     comicResults = [KJComic MR_findAll];
+    comicsForBrowser = [[NSMutableArray alloc] init];
     
+    for (KJComic *comic in comicResults) {
+        // NOTE - using thumbnails for now
+        // would have to check filepath for full-size comics
+        // refer to KJComicListView for filepath checking
+        [comicsForBrowser addObject:[MWPhoto photoWithImage:[UIImage imageWithData:comic.comicThumbData]]];
+        NSLog(@"comic file name: %@", comic.comicFileName);
+    }
+    
+    NSLog(@"comicsForBrowser array count: %lu", (unsigned long)[comicsForBrowser count]);
+    
+    [self reloadBrowserDataAndPresentBrowser];
+    
+    // TODO:
     // send a notification once fetch is complete
     // in that notification selector method, call reloadBrowserDataAndPresentBrowser
 }
 
 - (void)reloadBrowserDataAndPresentBrowser
 {
+    NSLog(@"now reloading browser data ...");
+    
     [browser reloadData];
     
-    // Present
+    // Present browser
     [self addChildViewController:browser];
     [[self view] addSubview:[browser view]];
     [browser didMoveToParentViewController:self];
@@ -70,13 +83,13 @@
 - (NSUInteger)numberOfPhotosInPhotoBrowser:(MWPhotoBrowser *)photoBrowser
 {
     // return comics array count
-    return comicResults.count;
+    return comicsForBrowser.count;
 }
 
 - (id <MWPhoto>)photoBrowser:(MWPhotoBrowser *)photoBrowser photoAtIndex:(NSUInteger)index
 {
-    if (index < comicResults.count) {
-        return [comicResults objectAtIndex:index];
+    if (index < comicsForBrowser.count) {
+        return [comicsForBrowser objectAtIndex:index];
     } else {
         return nil;
     }
@@ -84,9 +97,8 @@
 
 - (id <MWPhoto>)photoBrowser:(MWPhotoBrowser *)photoBrowser thumbPhotoAtIndex:(NSUInteger)index
 {
-    // NOTE - change this to the comic thumbs array
-    if (index < comicResults.count) {
-        return [comicResults objectAtIndex:index];
+    if (index < comicsForBrowser.count) {
+        return [comicsForBrowser objectAtIndex:index];
     } else {
         return nil;
     }
@@ -97,7 +109,6 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view.
     
     [self setupComicsBrowser];
 }
