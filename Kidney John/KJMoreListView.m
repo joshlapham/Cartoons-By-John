f67f0909494013e5eb2e913f8ccc46f84820b35a
Josh Lapham <josh@joshlapham.com>
2014-01-04 22:48:15 +1100

    Implemented PBWebViewController on social media detail view.

--- Kidney John/KJMoreListView.m
+++ Kidney John/KJMoreListView.m
@@ -8,6 +8,7 @@
 
 #import "KJMoreListView.h"
 #import "KJMoreDetailView.h"
+#import "PBWebViewController.h"
 
 @interface KJMoreListView ()
 
@@ -45,22 +46,85 @@
     return cell;
 }
 
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Check to see what link was clicked
+    // REVIEW: cause this code kinda sucks
+    NSURL *url = [[NSURL alloc] init];
+    
+    switch (indexPath.row) {
+        case 0:
+            url = [NSURL URLWithString:@"https://www.facebook.com/kidneyjohn"];
+            NSLog(@"URL IS: %@", url);
+            break;
+        
+        case 1:
+            url = [NSURL URLWithString:@"https://twitter.com/kidneyjohn"];
+            NSLog(@"URL IS: %@", url);
+            break;
+            
+        case 2:
+            url = [NSURL URLWithString:@"http://johnroderickpaine.tumblr.com"];
+            NSLog(@"URL IS: %@", url);
+            break;
+            
+        case 3:
+            url = [NSURL URLWithString:@"https://www.youtube.com/user/kidneyjohn"];
+            NSLog(@"URL IS: %@", url);
+            break;
+            
+        case 4:
+            url = [NSURL URLWithString:@"http://vimeo.com/johnroderickpaine"];
+            NSLog(@"URL IS: %@", url);
+            break;
+            
+        case 5:
+            url = [NSURL URLWithString:@"http://instagram.com/johnroderickpaine"];
+            NSLog(@"URL IS: %@", url);
+            break;
+            
+        default:
+            NSLog(@"NO URL");
+            break;
+    }
+    self.navigationItem.backBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:@"" style:UIBarButtonItemStylePlain target:nil action:nil];
+    
+    // Initialize the web view controller and set it's URL
+    PBWebViewController *webViewController = [[PBWebViewController alloc] init];
+    webViewController.URL = url;
+    
+    // These are custom UIActivity subclasses that will show up in the UIActivityViewController
+    // when the action button is clicked
+    //PBSafariActivity *activity = [[PBSafariActivity alloc] init];
+    //webViewController.applicationActivities = @[activity];
+    
+    // This property also corresponds to the same one on UIActivityViewController
+    // Both properties do not need to be set unless you want custom actions
+    //webViewController.excludedActivityTypes = @[UIActivityTypeMail, UIActivityTypeMessage, UIActivityTypePostToWeibo];
+    
+    // Hide tabbar on detail view
+    webViewController.hidesBottomBarWhenPushed = YES;
+    
+    // Push it
+    [self.navigationController pushViewController:webViewController animated:YES];
+}
+
 #pragma mark - Prepare for segue
 
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
-{
-    if ([segue.identifier isEqualToString:@"moreDetailSegue"]) {
-        // Set this in every view controller so that the back button displays back instead of the root view controller name
-        self.navigationItem.backBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:@"" style:UIBarButtonItemStylePlain target:nil action:nil];
-        
-        NSIndexPath *indexPath = [self.tableView indexPathForSelectedRow];
-        KJMoreDetailView *destViewController = segue.destinationViewController;
-        destViewController.nameFromList = [listItems objectAtIndex:indexPath.row];
-        
-        // Hide tabbar on detail view
-        destViewController.hidesBottomBarWhenPushed = YES;
-    }
-}
+//- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+//{
+//    if ([segue.identifier isEqualToString:@"moreDetailSegue"]) {
+//        // Set this in every view controller so that the back button displays back instead of the root view controller name
+//        self.navigationItem.backBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:@"" style:UIBarButtonItemStylePlain target:nil action:nil];
+//        
+//        NSIndexPath *indexPath = [self.tableView indexPathForSelectedRow];
+//        KJMoreDetailView *destViewController = segue.destinationViewController;
+//        destViewController.nameFromList = [listItems objectAtIndex:indexPath.row];
+//        
+//        // Hide tabbar on detail view
+//        destViewController.hidesBottomBarWhenPushed = YES;
+//    }
+//}
 
 #pragma mark - Init methods
 
