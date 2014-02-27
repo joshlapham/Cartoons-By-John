303baf880882ab98e0dec5f2892a03401104064b
Josh Lapham <josh@joshlapham.com>
2014-02-27 18:11:56 +1100

    Moved Social Media links VC to More VC in its own section.

--- Kidney John/KJMoreInitialView.m
+++ Kidney John/KJMoreInitialView.m
@@ -9,6 +9,7 @@
 #import "KJMoreInitialView.h"
 #import "KJFavouritesListView.h"
 #import "KJMoreListView.h"
+#import "PBWebViewController.h"
 
 @interface KJMoreInitialView () <UITableViewDataSource, UITableViewDelegate>
 
@@ -30,6 +31,22 @@
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
     // Return the number of rows in the section.
+    switch (section) {
+        case 0:
+            {
+                return 1;
+            }
+            break;
+        
+        case 1:
+            {
+                return [socialCellArray count];
+            }
+            break;
+            
+        default:
+            break;
+    }
     return 1;
 }
 
@@ -43,7 +60,7 @@
             
         case 1:
             //NSLog(@"section 1");
-            return [NSString stringWithFormat:@"Social Media"];
+            return [NSString stringWithFormat:@"Like, Comment, Subscribe"];
             break;
             
         default:
@@ -91,11 +108,37 @@
 {
     switch (indexPath.section) {
         case 0:
-            [self performSegueWithIdentifier:@"favouritesSegue" sender:self];
+            {
+                [self performSegueWithIdentifier:@"favouritesSegue" sender:self];
+            }
             break;
             
         case 1:
-            [self performSegueWithIdentifier:@"socialSegue" sender:self];
+            {
+                //[self performSegueWithIdentifier:@"socialSegue" sender:self];
+                
+                // Set back button to have no text
+                self.navigationItem.backBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:@"" style:UIBarButtonItemStylePlain target:nil action:nil];
+                
+                // Initialize the web view controller and set it's URL
+                PBWebViewController *webViewController = [[PBWebViewController alloc] init];
+                webViewController.URL = [self returnUrlForItemAtPath:indexPath];
+                
+                // These are custom UIActivity subclasses that will show up in the UIActivityViewController
+                // when the action button is clicked
+                //PBSafariActivity *activity = [[PBSafariActivity alloc] init];
+                //webViewController.applicationActivities = @[activity];
+                
+                // This property also corresponds to the same one on UIActivityViewController
+                // Both properties do not need to be set unless you want custom actions
+                //webViewController.excludedActivityTypes = @[UIActivityTypeMail, UIActivityTypeMessage, UIActivityTypePostToWeibo];
+                
+                // Hide tabbar on detail view
+                webViewController.hidesBottomBarWhenPushed = YES;
+                
+                // Push it
+                [self.navigationController pushViewController:webViewController animated:YES];
+            }
             break;
             
         default:
@@ -123,7 +166,8 @@
     //self.title = @"More";
     
     cellArray = [NSArray arrayWithObjects:@"Favourites List", nil];
-    socialCellArray = [NSArray arrayWithObjects:@"Like, Comment, Subscribe", nil];
+    socialCellArray = [NSArray arrayWithObjects:@"Facebook", @"Twitter", @"Tumblr", @"YouTube", @"Vimeo", @"Instagram", nil];
+
 }
 
 - (void)viewWillDisappear:(BOOL)animated
@@ -148,6 +192,44 @@
     // Dispose of any resources that can be recreated.
     
     cellArray = nil;
+}
+
+#pragma mark - Social media URL method
+
+- (NSURL *)returnUrlForItemAtPath:(NSIndexPath *)indexPath
+{
+    // Check to see what link was clicked
+    // REVIEW: cause this code kinda sucks
+    switch (indexPath.row) {
+        case 0:
+            return [NSURL URLWithString:@"https://www.facebook.com/kidneyjohn"];
+            break;
+            
+        case 1:
+            return [NSURL URLWithString:@"https://twitter.com/kidneyjohn"];
+            break;
+            
+        case 2:
+            return [NSURL URLWithString:@"http://johnroderickpaine.tumblr.com"];
+            break;
+            
+        case 3:
+            return [NSURL URLWithString:@"https://www.youtube.com/user/kidneyjohn"];
+            break;
+            
+        case 4:
+            return [NSURL URLWithString:@"http://vimeo.com/johnroderickpaine"];
+            break;
+            
+        case 5:
+            return [NSURL URLWithString:@"http://instagram.com/johnroderickpaine"];
+            break;
+            
+        default:
+            NSLog(@"Error: no URL");
+            return nil;
+            break;
+    }
 }
 
 @end
