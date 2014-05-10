a8dcac94237ff2f23e0301b69a87e91ff584bf92
Josh Lapham <josh@joshlapham.com>
2014-05-10 18:25:32 +1000

    Updated videoStore to prefetch video thumbnails if on wifi.

--- Kidney John/KJVideoStore.m
+++ Kidney John/KJVideoStore.m
@@ -11,6 +11,7 @@
 #import "KJVideo.h"
 #import "KJVideoFromParse.h"
 #import "JPLReachabilityManager.h"
+#import "SDWebImagePrefetcher.h"
 
 @implementation KJVideoStore
 
@@ -26,6 +27,28 @@
     });
     
     return _sharedStore;
+}
+
+#pragma mark - Prefetch video thumbnails method
+
++ (void)prefetchVideoThumbnails
+{
+    NSArray *resultsArray = [[NSArray alloc] init];
+    NSMutableArray *prefetchUrls = [[NSMutableArray alloc] init];
+    
+    resultsArray = [KJVideo MR_findAllSortedBy:@"videoDate" ascending:NO];
+    
+    for (KJVideo *video in resultsArray) {
+        NSString *urlString = [NSString stringWithFormat:@"https://img.youtube.com/vi/%@/default.jpg", video.videoId];
+        NSURL *urlToPrefetch = [NSURL URLWithString:urlString];
+        [prefetchUrls addObject:urlToPrefetch];
+    }
+    
+    // Cache URL for SDWebImage
+    [[SDWebImagePrefetcher sharedImagePrefetcher] prefetchURLs:prefetchUrls];
+    [[SDWebImagePrefetcher sharedImagePrefetcher] prefetchURLs:prefetchUrls progress:nil completed:^(NSUInteger finishedCount, NSUInteger skippedCount) {
+        DDLogVerbose(@"videoStore: prefetched video thumbs count: %d, skipped: %d", finishedCount, skippedCount);
+    }];
 }
 
 #pragma mark - Favourites methods
@@ -230,6 +253,12 @@
                 
                 NSString *notificationName = @"KJVideoDataFetchDidHappen";
                 [[NSNotificationCenter defaultCenter] postNotificationName:notificationName object:nil];
+                
+                // Prefetch video thumbnails if on Wifi
+                if ([JPLReachabilityManager isReachableViaWiFi]) {
+                    [KJVideoStore prefetchVideoThumbnails];
+                }
+                
             } else {
                 // Log details of the failure
                 DDLogVerbose(@"videoStore: error: %@ %@", error, [error userInfo]);
