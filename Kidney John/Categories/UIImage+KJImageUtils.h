b7eea2444367955c0fd6c6b23923bb3af3dd478c
Josh Lapham <josh@joshlapham.com>
2015-01-09 23:16:26 +1000

    Added KJImageUtils category for adding colour to tab bar icon images.

---
+++ Kidney John/Categories/UIImage+KJImageUtils.h
@@ -0,0 +1,15 @@
+//
+//  UIImage+KJImageUtils.h
+//  Kidney John
+//
+//  Created by jl on 9/01/2015.
+//  Copyright (c) 2015 Josh Lapham. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface UIImage (KJImageUtils)
+
+- (UIImage *)imageWithColor:(UIColor *)color1;
+
+@end
---
+++ Kidney John/Categories/UIImage+KJImageUtils.m
@@ -0,0 +1,29 @@
+//
+//  UIImage+KJImageUtils.m
+//  Kidney John
+//
+//  Created by jl on 9/01/2015.
+//  Copyright (c) 2015 Josh Lapham. All rights reserved.
+//
+
+#import "UIImage+KJImageUtils.h"
+
+@implementation UIImage (KJImageUtils)
+
+- (UIImage *)imageWithColor:(UIColor *)color1
+{
+    UIGraphicsBeginImageContextWithOptions(self.size, NO, self.scale);
+    CGContextRef context = UIGraphicsGetCurrentContext();
+    CGContextTranslateCTM(context, 0, self.size.height);
+    CGContextScaleCTM(context, 1.0, -1.0);
+    CGContextSetBlendMode(context, kCGBlendModeNormal);
+    CGRect rect = CGRectMake(0, 0, self.size.width, self.size.height);
+    CGContextClipToMask(context, rect, self.CGImage);
+    [color1 setFill];
+    CGContextFillRect(context, rect);
+    UIImage *newImage = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    return newImage;
+}
+
+@end
