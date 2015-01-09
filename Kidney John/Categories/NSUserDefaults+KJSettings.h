dd0cbd29476700dc430b863dd7302ed8fc0dc8b7
Josh Lapham <josh@joshlapham.com>
2015-01-09 20:30:37 +1000

    Added KJSettings category.

---
+++ Kidney John/Categories/NSUserDefaults+KJSettings.h
@@ -0,0 +1,13 @@
+//
+//  NSUserDefaults+KJSettings.h
+//  Kidney John
+//
+//  Created by jl on 9/01/2015.
+//  Copyright (c) 2015 Josh Lapham. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSUserDefaults_KJSettings : NSObject
+
+@end
---
+++ Kidney John/Categories/NSUserDefaults+KJSettings.m
@@ -0,0 +1,13 @@
+//
+//  NSUserDefaults+KJSettings.m
+//  Kidney John
+//
+//  Created by jl on 9/01/2015.
+//  Copyright (c) 2015 Josh Lapham. All rights reserved.
+//
+
+#import "NSUserDefaults+KJSettings.h"
+
+@implementation NSUserDefaults_KJSettings
+
+@end
