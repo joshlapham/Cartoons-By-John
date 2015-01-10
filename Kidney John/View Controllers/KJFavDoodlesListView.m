734af24f53d23c2114212e7f28ab4dff0298d693
Josh Lapham <josh@joshlapham.com>
2015-01-10 12:00:30 +1000

    Adhere KJFavDoodlesListView to style guide.

--- Kidney John/View Controllers/KJFavDoodlesListView.m
+++ Kidney John/View Controllers/KJFavDoodlesListView.m
@@ -13,50 +13,73 @@
 #import <SDWebImage/UIImageView+WebCache.h>
 #import "KJRandomView.h"
 
+// Constants
+static NSString *kDoodleFavouriteCellIdentifier = @"DoodleFavouriteCell";
+
 @interface KJFavDoodlesListView () <UICollectionViewDataSource, UICollectionViewDelegateFlowLayout, UICollectionViewDelegate, UIAlertViewDelegate>
 
 @property (weak, nonatomic) IBOutlet UICollectionView *collectionView;
+@property (nonatomic, strong) NSArray *cellResults;
 
 @end
 
-@implementation KJFavDoodlesListView {
-    NSArray *cellResults;
+@implementation KJFavDoodlesListView
+
+#pragma mark - viewDid methods
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    // Set title
+    self.title = NSLocalizedString(@"Doodles", @"Title of Doodles (drawings) favourites list view");
+    
+    // Register cell with collectionView
+    [self.collectionView registerClass:[KJDoodleCell class]
+            forCellWithReuseIdentifier:kDoodleFavouriteCellIdentifier];
+    
+    // Init data source array
+    // TODO: review this, init another way using Core Data
+    _cellResults = [KJDoodleStore returnFavouritesArray];
+    
+    // Check for Favourites results
+    if ([_cellResults count] == 0) {
+        [self thereAreNoFavourites];
+    }
+    
+    // Reload collectionView
+    [self.collectionView reloadData];
 }
 
 #pragma mark - UICollectionView delegate methods
 
-- (void)collectionView:(UICollectionView *)collectionView didSelectItemAtIndexPath:(NSIndexPath *)indexPath
-{
-    DDLogVerbose(@"Doodle Favs: selected item - %ld", (long)indexPath.row);
-    
+- (void)collectionView:(UICollectionView *)collectionView didSelectItemAtIndexPath:(NSIndexPath *)indexPath {
+//    DDLogVerbose(@"Doodle Favs: selected item - %ld", (long)indexPath.row);
     [self performSegueWithIdentifier:@"doodleDetailSegueFromFavourites" sender:self];
 }
 
-- (UIEdgeInsets)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout *)collectionViewLayout insetForSectionAtIndex:(NSInteger)section
-{
+- (UIEdgeInsets)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout *)collectionViewLayout insetForSectionAtIndex:(NSInteger)section {
     return UIEdgeInsetsMake(20, 20, 20, 20);
 }
 
-- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section
-{
-    return [cellResults count];
+- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
+    return [_cellResults count];
 }
 
-- (NSInteger)numberOfSectionsInCollectionView:(UICollectionView *)collectionView
-{
+- (NSInteger)numberOfSectionsInCollectionView:(UICollectionView *)collectionView {
     return 1;
 }
 
-- (CGSize)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout *)collectionViewLayout sizeForItemAtIndexPath:(NSIndexPath *)indexPath
-{
+- (CGSize)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout *)collectionViewLayout sizeForItemAtIndexPath:(NSIndexPath *)indexPath {
     return CGSizeMake(75, 75);
 }
 
-- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath
-{
-    KJDoodleCell *cell = (KJDoodleCell *)[collectionView dequeueReusableCellWithReuseIdentifier:@"DoodleFavouriteCell" forIndexPath:indexPath];
+- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath {
+    // Init cell
+    KJDoodleCell *cell = (KJDoodleCell *)[collectionView dequeueReusableCellWithReuseIdentifier:kDoodleFavouriteCellIdentifier
+                                                                                   forIndexPath:indexPath];
     
-    KJRandomImage *cellData = [cellResults objectAtIndex:indexPath.row];
+    // Init cell data
+    KJRandomImage *cellData = [_cellResults objectAtIndex:indexPath.row];
     
     // SDWebImage
     // Check if image is in cache
@@ -70,65 +93,46 @@
     return cell;
 }
 
-#pragma mark - UIAlertView delegate methods
 
-- (void)thereAreNoFavourites
-{
+- (void)thereAreNoFavourites {
+    // Init strings for noFavouritesAlertView
     NSString *titleString = NSLocalizedString(@"No Favourites", @"Title of error alert displayed when user hasn't favourited any items");
     NSString *messageString = NSLocalizedString(@"You haven't set any Doodles as favourites", @"Message displayed when user hasn't favourited any Doodles (drawings)");
     NSString *okButtonString = NSLocalizedString(@"OK", @"Title of OK button in No Favourites error alert");
     
-    UIAlertView *av = [[UIAlertView alloc] initWithTitle:titleString
+    // Init alertView
+    UIAlertView *noFavouritesAlertView = [[UIAlertView alloc] initWithTitle:titleString
                                                  message:messageString
                                                 delegate:self
                                        cancelButtonTitle:Nil
                                        otherButtonTitles:okButtonString, nil];
-    [av show];
+    
+    // Show alertView
+    [noFavouritesAlertView show];
 }
 
-- (void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex
-{
+#pragma mark - UIAlertView delegate methods
+
+- (void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex {
     [alertView dismissWithClickedButtonIndex:buttonIndex animated:YES];
     
+    // Go back to previous view controller
     [self.navigationController popViewControllerAnimated:YES];
 }
 
 #pragma mark - Prepare for segue method
 
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
-{
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
     // Set this in every view controller so that the back button displays back instead of the root view controller name
+    // TODO: review this, not really best practice
     self.navigationItem.backBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:@"" style:UIBarButtonItemStylePlain target:nil action:nil];
     
     if ([segue.identifier isEqualToString:@"doodleDetailSegueFromFavourites"]) {
         KJRandomView *destViewController = segue.destinationViewController;
         NSIndexPath *selectedIndex = [[self.collectionView indexPathsForSelectedItems] firstObject];
-        KJRandomImage *doodleCell = [cellResults objectAtIndex:selectedIndex.row];
-        
+        KJRandomImage *doodleCell = [_cellResults objectAtIndex:selectedIndex.row];
         destViewController.selectedImageFromFavouritesList = doodleCell;
     }
-}
-
-#pragma mark - Init methods
-
-- (void)viewDidLoad
-{
-    [super viewDidLoad];
-    
-    self.title = NSLocalizedString(@"Doodles", @"Title of Doodles (drawings) favourites list view");
-    
-    // Init collection view cell
-    [self.collectionView registerClass:[KJDoodleCell class] forCellWithReuseIdentifier:@"DoodleFavouriteCell"];
-    
-    cellResults = [KJDoodleStore returnFavouritesArray];
-    
-    // Check for Favourites results
-    if ([cellResults count] == 0) {
-        [self thereAreNoFavourites];
-    }
-    
-    // Reload collectionView
-    [self.collectionView reloadData];
 }
 
 @end
