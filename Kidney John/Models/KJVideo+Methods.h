de5d35992d031ceea99b46824444cececc4409ac
Josh Lapham <josh@joshlapham.com>
2015-01-10 11:45:44 +1000

    Added KJVideo+Methods category with YouTube thumbnail URL string constant.

---
+++ Kidney John/Models/KJVideo+Methods.h
@@ -0,0 +1,13 @@
+//
+//  KJVideo+Methods.h
+//  Kidney John
+//
+//  Created by jl on 10/01/2015.
+//  Copyright (c) 2015 Josh Lapham. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface KJVideo_Methods : NSObject
+
+@end
---
+++ Kidney John/Models/KJVideo+Methods.m
@@ -0,0 +1,13 @@
+//
+//  KJVideo+Methods.m
+//  Kidney John
+//
+//  Created by jl on 10/01/2015.
+//  Copyright (c) 2015 Josh Lapham. All rights reserved.
+//
+
+#import "KJVideo+Methods.h"
+
+@implementation KJVideo_Methods
+
+@end
