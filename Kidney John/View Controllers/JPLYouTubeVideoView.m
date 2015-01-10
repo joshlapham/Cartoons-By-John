c7d2efaea05a8aced163e8c2688cbdd3739113dc
Josh Lapham <josh@joshlapham.com>
2015-01-10 10:12:53 +1000

    Adhere JPLYouTubeVideoView to style guide.

--- Kidney John/View Controllers/JPLYouTubeVideoView.m
+++ Kidney John/View Controllers/JPLYouTubeVideoView.m
@@ -14,37 +14,86 @@
 #import "KJVideoStore.h"
 #import "JPLReachabilityManager.h"
 
+// Constants
+// YouTube video URL for social sharing
+static NSString *kYouTubeVideoUrlForSharing = @"https://www.youtube.com/watch?v=%@";
+
+// HTML string for YouTube video
+// NOTE - autoplay is set in playerVars
+// TODO: look at width and height variables in this string for Auto Layout issues
+static NSString *kYouTubeVideoHTML = @"<!DOCTYPE html><html><head><style>*{background-color:black;}body{margin:0px 0px 0px 0px;}</style><meta name = \"viewport\" content = \"initial-scale1.0, user-scalable=no\" /></head> <body> <div id=\"player\"></div> <script> var tag = document.createElement('script'); tag.src = \"http://www.youtube.com/player_api\"; var firstScriptTag = document.getElementsByTagName('script')[0]; firstScriptTag.parentNode.insertBefore(tag, firstScriptTag); var player; function onYouTubePlayerAPIReady() { player = new YT.Player('player', { playerVars: { autoplay: 0, showinfo: 0, rel: 0, modestbranding: 1, controls: 0 }, width:'1024', height:'704', videoId:'%@', events: { 'onReady': onPlayerReady, } }); } function onPlayerReady(event) { event.target.playVideo(); } </script> </body> </html>";
+
 @interface JPLYouTubeVideoView () <UIWebViewDelegate, UIAlertViewDelegate>
 
 @property (weak, nonatomic) IBOutlet UIWebView *videoView;
+@property (nonatomic, strong) MBProgressHUD *progressHud;
 
 @end
 
-@implementation JPLYouTubeVideoView {
-    MBProgressHUD *hud;
+@implementation JPLYouTubeVideoView
+
+#pragma mark - dealloc method
+
+- (void)dealloc {
+    [self.videoView setDelegate:nil];
+    [self.videoView stopLoading];
 }
 
-@synthesize videoIdFromList, videoTitleFromList;
+#pragma mark - viewDid methods
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    // Set title to video title
+    self.title = self.videoTitleFromList;
+    
+    // Init action button in top right hand corner
+    self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemAction
+                                                                                           target:self
+                                                                                           action:@selector(showActivityView)];
+    
+    // Init webView
+    _videoView.delegate = self;
+    _videoView.scrollView.scrollEnabled = NO;
+    
+    // Set to enable/disable autoplay
+    _videoView.mediaPlaybackRequiresUserAction = NO;
+    
+    // Call play video method
+    [self playVideoWithId:self.videoIdFromList];
+}
+
+- (void)viewDidDisappear:(BOOL)animated {
+    [super viewDidDisappear:YES];
+    
+    [self.videoView setDelegate:nil];
+    [self.videoView stopLoading];
+    self.videoView = nil;
+    
+    // Go back to video list if changing views
+    [self.navigationController popViewControllerAnimated:NO];
+}
 
 #pragma mark - UIAlertView delegate methods
 
-- (void)showErrorIfNoNetworkConnection
-{
+- (void)showErrorIfNoNetworkConnection {
+    // Init strings for noNetworkAlert
     NSString *titleString = NSLocalizedString(@"No Connection", @"Title of error alert displayed when no network connection is available");
     NSString *messageString = NSLocalizedString(@"A network connection is required to watch videos", @"Error message displayed when no network connection is available");
     NSString *okButtonString = NSLocalizedString(@"OK", @"Title of OK button in no network connection error alert");
     
+    // Init alertView
     UIAlertView *noNetworkAlert = [[UIAlertView alloc] initWithTitle:titleString
                                                              message:messageString
                                                             delegate:self
                                                    cancelButtonTitle:okButtonString
                                                    otherButtonTitles:nil, nil];
     
+    // Show alertView
     [noNetworkAlert show];
 }
 
-- (void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex
-{
+- (void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex {
     // If OK button was tapped ..
     if (buttonIndex == 0) {
         // Go back to video list view
@@ -54,11 +103,11 @@
 
 #pragma mark - UIWebView delegate methods
 
-- (void)webViewDidStartLoad:(UIWebView *)webView
-{
+- (void)webViewDidStartLoad:(UIWebView *)webView {
+    // If network is unreachable
     if ([JPLReachabilityManager isUnreachable]) {
         // Hide progress
-        [hud hide:YES];
+        [_progressHud hide:YES];
         
         // Hide network activity indicator
         [UIApplication sharedApplication].networkActivityIndicatorVisible = NO;
@@ -71,45 +120,36 @@
     }
 }
 
-- (void)webViewDidFinishLoad:(UIWebView *)webView
-{
+- (void)webViewDidFinishLoad:(UIWebView *)webView {
     // Hide progress
-    [hud hide:YES];
+    [_progressHud hide:YES];
     
     // Hide network activity indicator
     [UIApplication sharedApplication].networkActivityIndicatorVisible = NO;
 }
 
-- (void)webView:(UIWebView *)webView didFailLoadWithError:(NSError *)error
-{
+- (void)webView:(UIWebView *)webView didFailLoadWithError:(NSError *)error {
     // Hide progress
-    [hud hide:YES];
+    [_progressHud hide:YES];
     
     // Hide network activity indicator
     [UIApplication sharedApplication].networkActivityIndicatorVisible = NO;
     
-    DDLogVerbose(@"Video Detail: failed to load video: %@", [error localizedDescription]);
+    DDLogError(@"Video Detail VC: failed to load video: %@", [error localizedDescription]);
 }
-
-#pragma mark - HTML for YouTube webview
-
-// NOTE - autoplay is set in playerVars
-// TODO: look at width and height variables in this string for Auto Layout issues
-static NSString *youTubeVideoHTML = @"<!DOCTYPE html><html><head><style>*{background-color:black;}body{margin:0px 0px 0px 0px;}</style><meta name = \"viewport\" content = \"initial-scale1.0, user-scalable=no\" /></head> <body> <div id=\"player\"></div> <script> var tag = document.createElement('script'); tag.src = \"http://www.youtube.com/player_api\"; var firstScriptTag = document.getElementsByTagName('script')[0]; firstScriptTag.parentNode.insertBefore(tag, firstScriptTag); var player; function onYouTubePlayerAPIReady() { player = new YT.Player('player', { playerVars: { autoplay: 0, showinfo: 0, rel: 0, modestbranding: 1, controls: 0 }, width:'1024', height:'704', videoId:'%@', events: { 'onReady': onPlayerReady, } }); } function onPlayerReady(event) { event.target.playVideo(); } </script> </body> </html>";
 
 #pragma mark - Play video method
 
-- (void)playVideoWithId:(NSString *)videoId
-{
+- (void)playVideoWithId:(NSString *)videoId {
     // Show progress
-    hud = [MBProgressHUD showHUDAddedTo:self.view animated:YES];
-    hud.labelText = @"";
+    _progressHud = [MBProgressHUD showHUDAddedTo:self.view animated:YES];
+    _progressHud.labelText = @"";
     
     // Show network activity indicator
     [UIApplication sharedApplication].networkActivityIndicatorVisible = YES;
     
-    NSString *html = [NSString stringWithFormat:youTubeVideoHTML, videoId];
-    
+    // Init HTML for videoView
+    NSString *html = [NSString stringWithFormat:kYouTubeVideoHTML, videoId];
     [_videoView loadHTMLString:html baseURL:nil];
     
     // TODO: review these settings for Auto Layout issues
@@ -122,64 +162,27 @@ static NSString *youTubeVideoHTML = @"<!DOCTYPE html><html><head><style>*{backgr
 
 #pragma mark - UIActivityView methods
 
-- (void)showActivityView
-{
+- (void)showActivityView {
     NSString *titleString;
     
-    if (![KJVideoStore checkIfVideoIdIsAFavourite:videoIdFromList]) {
+    // TODO: refactor this as per Comics UIActivity
+    if (![KJVideoStore checkIfVideoIdIsAFavourite:self.videoIdFromList]) {
         titleString = NSLocalizedString(@"Add To Favourites", @"Title of button to favourite an item");
     } else {
         titleString = NSLocalizedString(@"Remove From Favourites", @"Title of button to remove an item as a favourite");
     }
     
-    KJVideoFavouriteActivity *favouriteActivity = [[KJVideoFavouriteActivity alloc] initWithActivityTitle:titleString andVideoId:videoIdFromList];
+    // Init UIActivity
+    KJVideoFavouriteActivity *favouriteActivity = [[KJVideoFavouriteActivity alloc] initWithActivityTitle:titleString
+                                                                                               andVideoId:self.videoIdFromList];
     
-    NSURL *activityUrl = [NSURL URLWithString:[NSString stringWithFormat:@"https://www.youtube.com/watch?v=%@", videoIdFromList]];
+    NSURL *activityUrl = [NSURL URLWithString:[NSString stringWithFormat:kYouTubeVideoUrlForSharing, self.videoIdFromList]];
     
-    UIActivityViewController *activityVC = [[UIActivityViewController alloc] initWithActivityItems:@[activityUrl] applicationActivities:@[favouriteActivity]];
+    UIActivityViewController *activityVC = [[UIActivityViewController alloc] initWithActivityItems:@[activityUrl]
+                                                                             applicationActivities:@[favouriteActivity]];
     
+    // Show UIActivity
     [self.navigationController presentViewController:activityVC animated:YES completion:nil];
-}
-
-#pragma mark - Init methods
-
-- (void)viewDidLoad
-{
-    [super viewDidLoad];
-    
-    // Set title to video title
-    self.title = videoTitleFromList;
-    
-    // Init action button in top right hand corner
-    self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemAction target:self action:@selector(showActivityView)];
-    
-    // Init webView
-    _videoView.delegate = self;
-    _videoView.scrollView.scrollEnabled = NO;
-    
-    // Set to enable/disable autoplay
-    _videoView.mediaPlaybackRequiresUserAction = NO;
-    
-    // Call play video method
-    [self playVideoWithId:videoIdFromList];
-}
-
-- (void)viewDidDisappear:(BOOL)animated
-{
-    [super viewDidDisappear:YES];
-    
-    [self.videoView setDelegate:nil];
-    [self.videoView stopLoading];
-    self.videoView = nil;
-    
-    // Go back to video list if changing views
-    [self.navigationController popViewControllerAnimated:NO];
-}
-
-- (void)dealloc
-{
-    [self.videoView setDelegate:nil];
-    [self.videoView stopLoading];
 }
 
 @end
