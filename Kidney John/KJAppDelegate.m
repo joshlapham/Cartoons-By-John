3c2a035d10af45e40fef844d455d3a91d9ebd728
Josh Lapham <josh@joshlapham.com>
2015-01-10 12:19:37 +1000

    Adhere KJAppDelegate to style guide.

--- Kidney John/KJAppDelegate.m
+++ Kidney John/KJAppDelegate.m
@@ -26,8 +26,7 @@
 
 #pragma mark - UI methods
 
-- (void)setupUI
-{
+- (void)setupUI {
     // TESTING - Version 2 colour scheme
 //    [NSUserDefaults kj_setUsingVersion2ColourSchemeSetting:NO];
     
@@ -57,7 +56,8 @@
     // TESTING - Version 2 colour scheme
     if (![NSUserDefaults kj_usingVersion2ColourSchemeSetting]) {
         [[UIApplication sharedApplication] setStatusBarStyle:UIStatusBarStyleLightContent];
-    } else {
+    }
+    else {
         // Version 2 colour
         [[UIApplication sharedApplication] setStatusBarStyle:UIStatusBarStyleDefault];
     }
@@ -102,8 +102,7 @@
 
 #pragma mark - Init methods
 
-- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
-{
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Init prefs for Settings
     NSDictionary *userDefaultsDefaults = @{ @"KJUsingVersion2ColourSchemeSetting" : [NSNumber numberWithBool:NO] };
     [[NSUserDefaults standardUserDefaults] registerDefaults:userDefaultsDefaults];
@@ -126,7 +125,8 @@
         // use registerUserNotificationSettings
         UIUserNotificationSettings *notificationSettings = [UIUserNotificationSettings settingsForTypes:UIUserNotificationTypeAlert | UIUserNotificationTypeBadge | UIUserNotificationTypeSound categories:nil];
         [application registerUserNotificationSettings:notificationSettings];
-    } else {
+    }
+    else {
         // iOS 7
         // use registerForRemoteNotifications
         [application registerForRemoteNotificationTypes:UIRemoteNotificationTypeAlert | UIRemoteNotificationTypeBadge | UIRemoteNotificationTypeSound];
@@ -168,7 +168,8 @@
     
     if ([JPLReachabilityManager isReachable]) {
         DDLogVerbose(@"Parse.com is reachable");
-    } else if ([JPLReachabilityManager isUnreachable]) {
+    }
+    else if ([JPLReachabilityManager isUnreachable]) {
         DDLogVerbose(@"Parse.com is NOT reachable");
     }
     
@@ -176,46 +177,39 @@
     return YES;
 }
 
-- (void)applicationWillResignActive:(UIApplication *)application
-{
+- (void)applicationWillResignActive:(UIApplication *)application {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
 }
 
-- (void)applicationDidEnterBackground:(UIApplication *)application
-{
+- (void)applicationDidEnterBackground:(UIApplication *)application {
     // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
 }
 
-- (void)applicationWillEnterForeground:(UIApplication *)application
-{
+- (void)applicationWillEnterForeground:(UIApplication *)application {
     // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
 }
 
-- (void)applicationDidBecomeActive:(UIApplication *)application
-{
+- (void)applicationDidBecomeActive:(UIApplication *)application {
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
 }
 
-- (void)applicationWillTerminate:(UIApplication *)application
-{
+- (void)applicationWillTerminate:(UIApplication *)application {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     [MagicalRecord cleanUp];
 }
 
 #pragma mark - Push notifications
 
-- (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)newDeviceToken
-{
+- (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)newDeviceToken {
     // Store the deviceToken in the current installation and save it to Parse.
     PFInstallation *currentInstallation = [PFInstallation currentInstallation];
     [currentInstallation setDeviceTokenFromData:newDeviceToken];
     [currentInstallation saveInBackground];
 }
 
-- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo
-{
+- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo {
     DDLogVerbose(@"Did receive push notification");
     
     [PFPush handlePush:userInfo];
