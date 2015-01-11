9431e3eeb1d5be021858e22538beedb53a9c3eac
Josh Lapham <josh@joshlapham.com>
2015-01-11 15:58:35 +1000

    Started implementing dynamic type for a future version.
    Squashed commit of the following:
    
    commit cdc9a02617db5ded3aac8112d67e414642c40050
    Author: Josh Lapham <josh@joshlapham.com>
    Date:   Sun Jan 11 15:58:14 2015 +1000
    
        Commented out fontWithDescriptor methods on KJFonts category.
    
    commit e1cab72234a1de49ca0fc0106e69f5a95fe0fad1
    Author: Josh Lapham <josh@joshlapham.com>
    Date:   Sun Jan 11 15:36:15 2015 +1000
    
        Continued implementing dynamic type.
    
    commit 69d451079d73081308edb374b723690c83112db3
    Author: Josh Lapham <josh@joshlapham.com>
    Date:   Sun Jan 11 15:11:00 2015 +1000
    
        Started implementing dynamic type with custom KJ font. Added UIFontDescriptor+JohnRoderickPaine category.
    
    commit 93ee13e3d374c5073e6edc98881a246d94c4678b
    Author: Josh Lapham <josh@joshlapham.com>
    Date:   Sun Jan 11 15:08:33 2015 +1000
    
        Update project file.

--- Kidney John/Categories/UIFont+KJFonts.m
+++ Kidney John/Categories/UIFont+KJFonts.m
@@ -7,12 +7,14 @@
 //
 
 #import "UIFont+KJFonts.h"
+#import "UIFontDescriptor+JohnRoderickPaine.h"
 
 @implementation UIFont (KJFonts)
 
 // Navbar
 + (UIFont *)kj_navbarFont {
     return [self kj_kidneyJohnFontOfSize:21];
+//    return [UIFont fontWithDescriptor:[UIFontDescriptor preferredJohnRoderickPaineFontDescriptorWithTextStyle:UIFontTextStyleHeadline] size: 0];
 }
 
 // Tab bar
@@ -28,27 +30,33 @@
 // Videos
 + (UIFont *)kj_videoNameFont {
     return [self kj_kidneyJohnFontOfSize:22];
+//    return [UIFont fontWithDescriptor:[UIFontDescriptor preferredJohnRoderickPaineFontDescriptorWithTextStyle:UIFontTextStyleHeadline] size: 0];
 }
 
 + (UIFont *)kj_videoDurationFont {
     return [self kj_kidneyJohnFontOfSize:18];
+//    return [UIFont fontWithDescriptor:[UIFontDescriptor preferredJohnRoderickPaineFontDescriptorWithTextStyle:UIFontTextStyleSubheadline] size: 0];
 }
 
 + (UIFont *)kj_videoNewLabelFont {
     return [self kj_kidneyJohnFontOfSize:12];
+//    return [UIFont fontWithDescriptor:[UIFontDescriptor preferredJohnRoderickPaineFontDescriptorWithTextStyle:UIFontTextStyleCaption2] size: 0];
 }
 
 + (UIFont *)kj_videoSearchBarFont {
     return [self kj_kidneyJohnFontOfSize:16];
+//    return [UIFont fontWithDescriptor:[UIFontDescriptor preferredJohnRoderickPaineFontDescriptorWithTextStyle:UIFontTextStyleBody] size: 0];
 }
 
 // 'More' view
 + (UIFont *)kj_sectionHeaderFont {
     return [self kj_kidneyJohnFontOfSize:17];
+//    return [UIFont fontWithDescriptor:[UIFontDescriptor preferredJohnRoderickPaineFontDescriptorWithTextStyle:UIFontTextStyleCaption1] size: 0];
 }
 
 + (UIFont *)kj_moreViewCellFont {
     return [self kj_kidneyJohnFontOfSize:20];
+//    return [UIFont fontWithDescriptor:[UIFontDescriptor preferredJohnRoderickPaineFontDescriptorWithTextStyle:UIFontTextStyleSubheadline] size: 0];
 }
 
 // Private methods
---
+++ Kidney John/Categories/UIFontDescriptor+JohnRoderickPaine.h
@@ -0,0 +1,17 @@
+//
+//  UIFontDescriptor+JohnRoderickPaine.h
+//  Kidney John
+//
+//  Created by jl on 11/01/2015.
+//  Copyright (c) 2015 Josh Lapham. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+extern NSString *const ANUIFontTextStyleCaption3;
+
+@interface UIFontDescriptor (JohnRoderickPaine)
+
++(UIFontDescriptor *)preferredJohnRoderickPaineFontDescriptorWithTextStyle:(NSString *)style;
+
+@end
