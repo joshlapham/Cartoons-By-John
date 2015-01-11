bde55eb6dea471e94c968b3f3a1b9be887335e87
Josh Lapham <josh@joshlapham.com>
2015-01-11 20:56:45 +1000

    Added KJSocialLink+Methods category with parseClassName helper method.

---
+++ Kidney John/Models/KJSocialLink+Methods.h
@@ -0,0 +1,17 @@
+//
+//  KJSocialLink+Methods.h
+//  Kidney John
+//
+//  Created by jl on 11/01/2015.
+//  Copyright (c) 2015 Josh Lapham. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "KJSocialLink.h"
+
+@interface KJSocialLink (Methods)
+
+// Parse helper methods
++ (NSString *)parseClassName;
+
+@end
---
+++ Kidney John/Models/KJSocialLink+Methods.m
@@ -0,0 +1,18 @@
+//
+//  KJSocialLink+Methods.m
+//  Kidney John
+//
+//  Created by jl on 11/01/2015.
+//  Copyright (c) 2015 Josh Lapham. All rights reserved.
+//
+
+#import "KJSocialLink+Methods.h"
+
+@implementation KJSocialLink (Methods)
+
+// Parse helper methods
++ (NSString *)parseClassName {
+    return @"SocialLink";
+}
+
+@end
