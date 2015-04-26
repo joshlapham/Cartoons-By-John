2f1f94cae3a8223a86a5b020956d28a3a47cd56b
Josh Lapham <josh@joshlapham.com>
2015-04-26 11:11:01 +1000

    Add Social Link cell class files to project.

---
+++ Kidney John/Cells/KJSocialLinkCell.m
@@ -0,0 +1,25 @@
+//
+//  KJSocialLinkCell.m
+//  Kidney John
+//
+//  Created by jl on 26/04/2015.
+//  Copyright (c) 2015 Josh Lapham. All rights reserved.
+//
+
+#import "KJSocialLinkCell.h"
+
+@implementation KJSocialLinkCell
+
+- (void)awakeFromNib {
+    // Initialization code
+    
+    NSLog(@"AWAKE FROM NIB");
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
