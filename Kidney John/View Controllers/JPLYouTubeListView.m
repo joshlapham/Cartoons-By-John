c6391a67b6d0b85501f0d9be04f52e14e0071dc7
Josh Lapham <josh@joshlapham.com>
2015-04-30 16:35:34 +1000

    Implement showFatalErrorAlert method on Video List VC.

--- Kidney John/View Controllers/JPLYouTubeListView.m
+++ Kidney John/View Controllers/JPLYouTubeListView.m
@@ -329,6 +329,32 @@
     return fetchedResultsController;
 }
 
+#pragma mark - Show fatal error alert method
+
+- (void)showFatalErrorAlert {
+    // Init alert strings
+    NSString *alertTitle = NSLocalizedString(@"Error", nil);
+    NSString *alertMessage = NSLocalizedString(@"We're sorry, a fatal error occurred. Please try exiting and re-launching the app.", nil);
+    NSString *okayButtonTitle = NSLocalizedString(@"Okay", nil);
+    
+    // Init alert
+    UIAlertController *errorAlert = [UIAlertController alertControllerWithTitle:alertTitle
+                                                                        message:alertMessage
+                                                                 preferredStyle:UIAlertControllerStyleAlert];
+    
+    // Init actions
+    UIAlertAction *okayAction = [UIAlertAction actionWithTitle:okayButtonTitle
+                                                         style:UIAlertActionStyleDefault
+                                                       handler:nil];
+    
+    [errorAlert addAction:okayAction];
+    
+    // Show alert
+    [self presentViewController:errorAlert
+                       animated:YES
+                     completion:nil];
+}
+
 - (NSFetchedResultsController *)fetchedResultsController {
     if (_fetchedResultsController != nil) {
         return _fetchedResultsController;
