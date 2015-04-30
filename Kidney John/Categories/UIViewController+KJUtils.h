0f1d9e9dd309202fa8b1ee21af1422aefdf2d28b
Josh Lapham <josh@joshlapham.com>
2015-04-30 16:36:48 +1000

    Add UIViewController+KJUtils category to project.

---
+++ Kidney John/Categories/UIViewController+KJUtils.h
@@ -0,0 +1,13 @@
+//
+//  UIViewController+KJUtils.h
+//  Kidney John
+//
+//  Created by Josh Lapham on 30/04/2015.
+//  Copyright (c) 2015 Josh Lapham. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface UIViewController (KJUtils)
+
+@end
---
+++ Kidney John/Categories/UIViewController+KJUtils.m
@@ -0,0 +1,13 @@
+//
+//  UIViewController+KJUtils.m
+//  Kidney John
+//
+//  Created by Josh Lapham on 30/04/2015.
+//  Copyright (c) 2015 Josh Lapham. All rights reserved.
+//
+
+#import "UIViewController+KJUtils.h"
+
+@implementation UIViewController (KJUtils)
+
+@end
