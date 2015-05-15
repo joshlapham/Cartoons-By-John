80f4838f1941f669301c9ddabe065f8a89d94f83
Josh Lapham <josh@joshlapham.com>
2015-05-16 08:00:44 +1000

    Add Secret Admin VC class files to project.

---
+++ Kidney John/View Controllers/Admin/KJSecretAdminViewController.h
@@ -0,0 +1,13 @@
+//
+//  KJSecretAdminViewController.h
+//  Kidney John
+//
+//  Created by jl on 16/05/2015.
+//  Copyright (c) 2015 Josh Lapham. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KJSecretAdminViewController : UIViewController
+
+@end
---
+++ Kidney John/View Controllers/Admin/KJSecretAdminViewController.m
@@ -0,0 +1,37 @@
+//
+//  KJSecretAdminViewController.m
+//  Kidney John
+//
+//  Created by jl on 16/05/2015.
+//  Copyright (c) 2015 Josh Lapham. All rights reserved.
+//
+
+#import "KJSecretAdminViewController.h"
+
+@interface KJSecretAdminViewController ()
+
+@end
+
+@implementation KJSecretAdminViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
