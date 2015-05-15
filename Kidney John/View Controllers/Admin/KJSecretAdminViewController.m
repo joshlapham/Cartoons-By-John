0921583a5a907dae102537cc58d7451f81a940ab
Josh Lapham <josh@joshlapham.com>
2015-05-16 08:52:40 +1000

    Continue implementing Secret Admin VC.

--- Kidney John/View Controllers/Admin/KJSecretAdminViewController.m
+++ Kidney John/View Controllers/Admin/KJSecretAdminViewController.m
@@ -26,12 +26,26 @@
     
     // Prevent segmented control from being hidden
     self.navigationController.navigationBar.translucent = NO;
+    
+    // Init 'Done' navbar button
+    UIBarButtonItem *doneButton = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemDone
+                                                                                target:self
+                                                                                action:@selector(didTapDoneButton:)];
+    self.navigationItem.leftBarButtonItem = doneButton;
 }
 
 #pragma mark - UIToolbarDelegate methods
 
 - (UIBarPosition)positionForBar:(id<UIBarPositioning>)bar {
     return UIBarPositionTop;
+}
+
+#pragma mark - Action handler methods
+
+- (IBAction)didTapDoneButton:(id)sender {
+    // TODO: fix this
+    // Go back to previous view (back to the app)
+    [self.navigationController popToRootViewControllerAnimated:YES];
 }
 
 @end
