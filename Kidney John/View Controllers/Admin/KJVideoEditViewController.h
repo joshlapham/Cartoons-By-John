8b66539997d30e1b1d139fd4ab2b2dee2498dd39
Josh Lapham <josh@joshlapham.com>
2015-05-16 16:29:18 +1000

    Add Video Edit VC class files to project.

---
+++ Kidney John/View Controllers/Admin/KJVideoEditViewController.h
@@ -0,0 +1,13 @@
+//
+//  KJVideoEditViewController.h
+//  Kidney John
+//
+//  Created by Josh Lapham on 16/05/2015.
+//  Copyright (c) 2015 Josh Lapham. All rights reserved.
+//
+
+#import "QuickDialogController.h"
+
+@interface KJVideoEditViewController : QuickDialogController
+
+@end
---
+++ Kidney John/View Controllers/Admin/KJVideoEditViewController.m
@@ -0,0 +1,28 @@
+//
+//  KJVideoEditViewController.m
+//  Kidney John
+//
+//  Created by Josh Lapham on 16/05/2015.
+//  Copyright (c) 2015 Josh Lapham. All rights reserved.
+//
+
+#import "KJVideoEditViewController.h"
+
+@interface KJVideoEditViewController ()
+
+@end
+
+@implementation KJVideoEditViewController
+
+#pragma mark - dealloc method
+
+- (void)dealloc {
+}
+
+#pragma mark - View lifecycle methods
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+@end
