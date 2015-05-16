793053acea9f028e85722997a6ae8b25c0af5422
Josh Lapham <josh@joshlapham.com>
2015-05-16 11:05:57 +1000

    Add KJBaseDataSource class files to project.

---
+++ Kidney John/Data Sources/KJBaseDataSource.h
@@ -0,0 +1,16 @@
+//
+//  KJBaseDataSource.h
+//  Kidney John
+//
+//  Created by Josh Lapham on 16/05/2015.
+//  Copyright (c) 2015 Josh Lapham. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface KJBaseDataSource : NSObject
+
+// Properties
+@property (nonatomic, strong) NSArray *cellDataSource;
+
+@end
