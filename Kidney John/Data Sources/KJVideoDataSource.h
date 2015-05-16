4d0f87df3672965c6c599e466d37a10fc7cff750
Josh Lapham <josh@joshlapham.com>
2015-05-16 11:06:47 +1000

    Add KJVideoDataSource class files to project.

---
+++ Kidney John/Data Sources/KJVideoDataSource.h
@@ -0,0 +1,13 @@
+//
+//  KJVideoDataSource.h
+//  Kidney John
+//
+//  Created by Josh Lapham on 16/05/2015.
+//  Copyright (c) 2015 Josh Lapham. All rights reserved.
+//
+
+#import "KJBaseDataSource.h"
+
+@interface KJVideoDataSource : KJBaseDataSource
+
+@end
---
+++ Kidney John/Data Sources/KJVideoDataSource.m
@@ -0,0 +1,13 @@
+//
+//  KJVideoDataSource.m
+//  Kidney John
+//
+//  Created by Josh Lapham on 16/05/2015.
+//  Copyright (c) 2015 Josh Lapham. All rights reserved.
+//
+
+#import "KJVideoDataSource.h"
+
+@implementation KJVideoDataSource
+
+@end
