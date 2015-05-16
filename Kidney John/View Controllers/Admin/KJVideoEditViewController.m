59f93a9d7f6bf80fe607ccb3d6b48058a74c138d
Josh Lapham <josh@joshlapham.com>
2015-05-16 16:32:38 +1000

    Continue implementing Video Edit VC.

--- Kidney John/View Controllers/Admin/KJVideoEditViewController.m
+++ Kidney John/View Controllers/Admin/KJVideoEditViewController.m
@@ -7,6 +7,11 @@
 //
 
 #import "KJVideoEditViewController.h"
+#import <QRootElement.h>
+#import <QSection.h>
+#import <QLabelElement.h>
+#import <QEntryElement.h>
+#import <QBooleanElement.h>
 
 @interface KJVideoEditViewController ()
 
@@ -19,10 +24,54 @@
 - (void)dealloc {
 }
 
+#pragma mark - Init method
+
+- (instancetype)init {
+    self = [super init];
+    if (self) {
+        self.root = [self buildRoot];
+        self.resizeWhenKeyboardPresented = YES;
+    }
+    
+    return self;
+}
+
 #pragma mark - View lifecycle methods
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+}
+
+#pragma mark - QuickDialog methods
+
+#pragma mark Build root method
+
+- (QRootElement *)buildRoot {
+    QRootElement *root = [[QRootElement alloc] init];
+    root.grouped = YES;
+    
+    // TODO: set title
+    //    root.title = @"More";
+    
+    // Section
+    {
+        QSection *section = [[QSection alloc] init];
+        
+        // My Profile
+        {
+            QLabelElement *element = [[QLabelElement alloc] init];
+            //            element.title = @"My Profile";
+            //            element.controllerAction = @"openProfile";
+            element.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+            
+            [section addElement:element];
+        }
+        
+        // Add all to root
+        [root addSection:section];
+    }
+    
+    return root;
 }
 
 @end
