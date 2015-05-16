5c9300fd2588c55a578f4b00209d363350d1cccf
Josh Lapham <josh@joshlapham.com>
2015-05-16 11:20:07 +1000

    Add KJVideoCollectionViewCell class files to project.

---
+++ Kidney John/Cells/KJVideoCollectionViewCell.m
@@ -0,0 +1,21 @@
+//
+//  KJVideoCollectionViewCell.m
+//  Kidney John
+//
+//  Created by Josh Lapham on 16/05/2015.
+//  Copyright (c) 2015 Josh Lapham. All rights reserved.
+//
+
+#import "KJVideoCollectionViewCell.h"
+
+@implementation KJVideoCollectionViewCell
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
