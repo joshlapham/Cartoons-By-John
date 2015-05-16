13cb3d5b36c4d75499afbe6ff8149180dfe54711
Josh Lapham <josh@joshlapham.com>
2015-05-17 09:18:25 +1000

    Add video thumbnail to Video collectionView cell.

--- Kidney John/Cells/KJVideoCollectionViewCell.m
+++ Kidney John/Cells/KJVideoCollectionViewCell.m
@@ -8,6 +8,8 @@
 
 #import "KJVideoCollectionViewCell.h"
 #import <Parse/Parse.h>
+#import <SDWebImage/UIImageView+WebCache.h>
+#import "KJVideo+Methods.h"
 
 @interface KJVideoCollectionViewCell ()
 
@@ -15,6 +17,7 @@
 @property (weak, nonatomic) IBOutlet UILabel *nameLabel;
 @property (weak, nonatomic) IBOutlet UILabel *descriptionLabel;
 @property (weak, nonatomic) IBOutlet UILabel *durationLabel;
+@property (weak, nonatomic) IBOutlet UIImageView *thumbnailView;
 
 @end
 
@@ -24,13 +27,13 @@
 //
 //- (void)awakeFromNib {
 //    [super awakeFromNib];
-//    
+//
 //    NSLog(@"%@ - %s", [self class], __func__);
-//    
+//
 //    // TODO: implement
-//    
+//
 //    self.contentView.backgroundColor = [UIColor whiteColor];
-//    
+//
 //    _descriptionLabel = [[UILabel alloc] initWithFrame:CGRectMake(CGRectGetMidX(self.bounds), CGRectGetMidY(self.bounds), 100, 50)];
 //    [_descriptionLabel setTextColor:[UIColor blackColor]];
 //    [self.contentView addSubview:_descriptionLabel];
@@ -44,11 +47,11 @@
     
     self.contentView.backgroundColor = [UIColor whiteColor];
     
-//    _descriptionLabel = [[UILabel alloc] initWithFrame:CGRectMake(CGRectGetMidX(self.bounds), CGRectGetMidY(self.bounds), 100, 50)];
+    //    _descriptionLabel = [[UILabel alloc] initWithFrame:CGRectMake(CGRectGetMidX(self.bounds), CGRectGetMidY(self.bounds), 100, 50)];
     [_nameLabel setTextColor:[UIColor blackColor]];
     [_descriptionLabel setTextColor:[UIColor blackColor]];
     [_durationLabel setTextColor:[UIColor blackColor]];
-//    [self addSubview:_descriptionLabel];
+    //    [self addSubview:_descriptionLabel];
     
     // Init cell data
     PFObject *cellData = (PFObject *)data;
@@ -57,6 +60,18 @@
     _nameLabel.text = [cellData valueForKey:@"videoName"];
     _descriptionLabel.text = [cellData valueForKey:@"date"];
     _durationLabel.text = [cellData valueForKey:@"videoDuration"];
+    
+    NSString *urlString = [NSString stringWithFormat:KJYouTubeVideoThumbnailUrlString, [cellData valueForKey:@"videoId"]];
+    
+    [_thumbnailView sd_setImageWithURL:[NSURL URLWithString:urlString]
+                      placeholderImage:[UIImage imageNamed:@"placeholder.png"]
+                             completed:^(UIImage *cellImage, NSError *error, SDImageCacheType cacheType, NSURL *url) {
+                                 if (cellImage && !error) {
+                                     DDLogVerbose(@"%s: fetched video thumbnail image from URL: %@", __func__, url);
+                                 } else {
+                                     DDLogError(@"%s: error fetching video thumbnail image: %@", __func__, [error localizedDescription]);
+                                 }
+                             }];
 }
 
 @end
